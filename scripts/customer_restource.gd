cfcdadec5ee7e0168ec9867c903ddc806ef17b6d
cipher wave <wavemfw@gmail.com>
2025-10-27 13:05:34 +0100

    added resource for client

---
+++ scripts/customer_restource.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name CustomerResource
+
+@export var name: String
+@export var condition: String
+@export var price: int
+@export var portrait: Texture2D
+@export var insured: bool
+@export var lie: String
