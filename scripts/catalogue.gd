0c45906cde064a03ac1b2e37caf55f3c391aaa8a
AgonGuri <guriagon@gmail.com>
2025-10-27 12:15:04 +0100

    its not running :(
    something something subprocesses

---
+++ scripts/catalogue.gd
@@ -0,0 +1,17 @@
+extends TextureButton
+
+@onready var openCatalogue = $openCatalogue
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func turnOffCanvas() -> void:
+	openCatalogue.visible = false
+
+func _pressed() -> void:
+	openCatalogue.visible = true
---
+++ scripts/game_scene.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+@onready var catalogue = $catalogue
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _input(_event: InputEvent) -> void:
+	#turn off the catalogue if it is open
+	if (catalogue.openCatalogue.visible == true):
+		catalogue.turnOffCanvas()
