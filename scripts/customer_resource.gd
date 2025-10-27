409ff78ebc7917066e976ac56486a9bfadead787
AgonGuri <guriagon@gmail.com>
2025-10-27 18:30:22 +0100

    added max_payout to the customer_recource

--- scripts/customer_resource.gd
+++ scripts/customer_resource.gd
@@ -6,6 +6,7 @@ class_name CustomerResource
 @export var portrait: Texture2D
 @export var condition: String
 @export var price: int
+@export var max_payout: int
 @export var insured: bool
 @export var is_lying: bool
 @export var lie: String
