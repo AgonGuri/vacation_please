54dddb3938fee16c98a519accc4d7fffa768231a
AgonGuri <guriagon@gmail.com>
2025-10-28 12:05:17 +0100

    made reward calculation work

--- scripts/main_layout.gd
+++ scripts/main_layout.gd
@@ -30,3 +30,38 @@ func _input(event: InputEvent) -> void:
 	#turn off the catalogue if it is open
 	if catalogue.openCatalogue.visible == true && event.is_pressed():
 		catalogue.closeCanvas()
+		
+		# calculation outcomes
+var COMMISSION_PERCENTAGE = 0.15
+var BONUS_PERCENTAGE = 0.4
+var COURT_COST = 2000
+
+func commission(money):
+	return money * COMMISSION_PERCENTAGE
+
+func bonus(money):
+	return money * BONUS_PERCENTAGE
+
+func _on_main_ui_money_sent(customer: CustomerResource, amount: float) -> void:
+	print("got into the signal at least")
+	if customer.is_lying: #if hes lying
+		if randf() < .5:	#50% chance of max payout being 0 meaning lie gets out
+			if amount > 0 :
+				Global.currency -= amount
+				print("boss: you pay for that liar!")
+			else:print("we aint giving money to liars!")
+		else: print("*that lie didt get out, you got lucky*")
+	if amount == customer.max_payout : #handeled customer well
+		Global.currency += commission(amount)
+		print("boss: customer well handeled, heres the commission")
+	if amount > customer.max_payout: #pay out of own pocket
+		Global.currency -= (amount - customer.max_payout)
+		print("boss: no commission for you and you are paying everything over the max payout!")
+	if amount < customer.max_payout: #get bonus since we save money
+		Global.currency += commission(amount) + bonus(customer.max_payout - amount)
+		print("boss: nice you saved us some money, here is a bonus for the good work!")
+		if amount < customer.price: #the less we give them the higher the chance of being sued
+			if randf() < (customer.max_payout - amount) / customer.max_payout:
+				#rest of what they could have gotten + court
+				print("you got sued and have to pay: ", (customer.max_payout - amount + COURT_COST))
+				Global.currency -= customer.max_payout - amount + COURT_COST
