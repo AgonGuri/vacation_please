0d7b083cd8254ef928300cc143415cfa1daabf42
AgonGuri <guriagon@gmail.com>
2025-10-28 10:37:09 +0100

    updated max payouts

--- scripts/main_layout.gd
+++ scripts/main_layout.gd
@@ -16,8 +16,6 @@ func _ready() -> void:
 func new_customer():
 	Global.next_customer()
 	current_customer = Global.client_dict[Global.current_customer]
-	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -27,3 +25,7 @@ func _input(event: InputEvent) -> void:
 	#turn off the catalogue if it is open
 	if catalogue.openCatalogue.visible == true && event.is_pressed():
 		catalogue.closeCanvas()
+
+
+func _on_main_ui_money_sent(customer: CustomerResource, amount: float) -> void:
+	if customer.
